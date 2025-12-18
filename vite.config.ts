641d3fea9a37f7ea6e5c21832a25b93ae15d7d8f
Datheen <praydatheen@gmail.com>
2025-12-18 15:33:41 -0400

    init project

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Projeto-CETAM/",
+  base: "/CETAM/",
   plugins: [tailwindcss(), react()],
 });
